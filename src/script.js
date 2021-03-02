56463bb92fdb222c4e1bbadd5bbdbd0bee865ca6
Matthias Steiner <m@kjpjm.de>
2021-03-02 22:26:49 +0100

    add ids

--- src/script.js
+++ src/script.js
@@ -8,6 +8,8 @@ const SEARCH_URL =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
 const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 //Get init
 getMovies(API_URL);
